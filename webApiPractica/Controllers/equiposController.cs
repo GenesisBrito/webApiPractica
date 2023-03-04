843c71437f0ef73ae4f80161b9f35105591e9600
genes <genes@DESKTOP-5E44V4S>
2023-03-04 03:47:45 -0600

    modificación

--- webApiPractica/Controllers/equiposController.cs
+++ webApiPractica/Controllers/equiposController.cs
@@ -1,9 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using webApiPractica.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Hosting.Server;
-using System.Diagnostics.Metrics;
+using webApiPractica.Models;
 
 namespace webApiPractica.Properties
 {
@@ -21,20 +18,116 @@ namespace webApiPractica.Properties
         /// <summary>
         /// </summary>
         /// <returns></returns>
-        [HttpGet] 
+        [HttpGet]
 
         [Route("GetAll")]
         public IActionResult Get()
         {
-            List<equipos> listadoEquipo = ( from e in _equiposContexto.equipos select e ).ToList();
+            List<equipos> listadoEquipo = (from e in _equiposContexto.equipos select e).ToList();
 
-            if (listadoEquipo.Count() = 0)
+            if (listadoEquipo.Count == 0)
             {
                 return NotFound();
             }
 
             return Ok(listadoEquipo);
 
+        }
+        [HttpGet]
+        [Route("GetById/{id}")]
+
+        public IActionResult Get(int id)
+        {
+            equipos? equipo = (from e in _equiposContexto.equipos
+                               where e.id_equipos == id
+                               select e).FirstOrDefault();
+
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+            return Ok(equipo);
+
+        }
+        [HttpGet]
+        [Route("Find/{filtro}")]
+
+        public IActionResult FindbyDescription(String filtro)
+        {
+            equipos? equipo = (from e in _equiposContexto.equipos
+                               where e.descripcion.Contains(filtro)
+                               select e).FirstOrDefault();
+
+            if (equipo == null)
+            {
+                return NotFound();
+            }
+            return Ok(equipo);
+        }
+
+        [HttpPost]
+        [Route("Add")]
+
+        public IActionResult GuardarEquipo([FromBody] equipos equipo)
+        {
+
+            try
+            {
+                _equiposContexto.equipos.Add(equipo);
+                _equiposContexto.SaveChanges();
+                return Ok(equipo);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+
+            }
+
+        }
+
+        [HttpPut]
+        [Route("actualizar/{id}")]
+
+        public IActionResult ActualizarEquipo(int id, [FromBody] equipos equipoModificar)
+        {
+            equipos? equiposActual = (from e in _equiposContexto.equipos
+                                      where e.id_equipos == id
+                                      select e).FirstOrDefault();
+            if (equiposActual == null)
+            {
+                return NotFound(id);
+            }
+
+            equiposActual.nombre = equipoModificar.nombre;
+            equiposActual.descripcion = equipoModificar.descripcion;
+            equiposActual.marca_id = equipoModificar.marca_id;
+            equiposActual.tipo_equipo_id = equipoModificar.tipo_equipo_id;
+            equiposActual.anio_compra = equipoModificar.anio_compra;
+            equiposActual.costo = equipoModificar.costo;
+
+            _equiposContexto.Entry(equiposActual).State = EntityState.Modified;
+            _equiposContexto.SaveChanges();
+            return Ok(equipoModificar);
+        }
+
+        [HttpDelete]
+        [Route("eliminar/{id}")]
+
+        public IActionResult EliminarEquipo(int id)
+        {
+
+            equipos? equipo = (from e in _equiposContexto.equipos
+                               where e.id_equipos == id
+                               select e).FirstOrDefault();
+
+            if (equipo == null)
+                return NotFound();
+
+            _equiposContexto.equipos.Attach(equipo);
+            _equiposContexto.equipos.Remove(equipo);
+            _equiposContexto.SaveChanges();
+
+            return Ok(equipo);
         }
     }
 
